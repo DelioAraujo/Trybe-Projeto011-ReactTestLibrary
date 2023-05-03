d6f406564d8043fc58df5fa1fcbcec3f0c8d239c
DelioAraujo <delioaraujo@gmail.com>
2023-05-03 18:59:37 -0300

    item 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+  renderWithRouter(<FavoritePokemon />);
+
+  const mensagem = screen.getByText(/no favorite pokémon found/i);
+
+  expect(mensagem).toBeInTheDocument();
+});
+
+test('Teste se apenas são exibidos os Pokémon favoritados.', () => {
+  renderWithRouter(<App />);
+
+  const linkDetails = screen.getByRole('link', { name: /more details/i });
+
+  userEvent.click(linkDetails);
+
+  const favoriteCheckbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+
+  userEvent.click(favoriteCheckbox);
+
+  const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+
+  userEvent.click(favoriteLink);
+
+  const favoritePokemon = screen.getByText(/pikachu/i);
+
+  expect(favoritePokemon).toBeInTheDocument();
+});
